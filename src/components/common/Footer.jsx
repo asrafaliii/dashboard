851d516caa8ac7c379a06d7766218b4be9b9cc77
John Doe <toolsware.xyz@gmail.com>
2023-06-05 16:34:16 +0600

    update

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
       }}
     >
       <Typography>
-        Created by <span style={{ color: '#027edd' }}>BSoftware</span> | All
+        Created by <span style={{ color: '#027edd' }}>BSoftware</span> All
         Rights Reserved &copy;
         {new Date().getFullYear()}
       </Typography>
